8717f13bd26d4daf28a9caeb83b680d5d41a0b7f
Yael <yaelsilvestre64@gmail.com>
2024-11-25 20:24:29 -0600

    Revert "Mejora de page inventario"
    This reverts commit ad224c2133fd33c58a898c8d8920254ebfbfa571.

--- src/app/pages/inventario/inventario.page.ts
+++ src/app/pages/inventario/inventario.page.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductService, Producto } from '../../services/product.service';
 import { CategoriaService } from '../../services/categoria.service'; // Servicio para categorías
 import { ProveedoresService } from '../../services/proveedores.service'; // Servicio para proveedores
 import { ToastController, AlertController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
+=======
+>>>>>>> parent of ad224c2 (Mejora de page inventario)
 import { ProductService } from '../../services/product.service';
 import { NavController, ModalController, AlertController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
@@ -12,6 +15,20 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { CategoriaService } from '../../services/categoria.service';  // Verifica que la ruta del archivo sea correcta
  // Asegúrate de importar el servicio de categorías
 import { ProveedoresService } from '../../services/proveedores.service'; // Asegúrate de importar el servicio de proveedores
+<<<<<<< HEAD
+=======
+
+export interface Producto {
+  id?: string;
+  Nombre: string;
+  Precio: number;
+  Stock: number;
+  Categoria: string;
+  Descripcion: string;
+  Fechadeagregado: any;
+  proveedor: string;
+}
+>>>>>>> parent of ad224c2 (Mejora de page inventario)
 
 export interface Producto {
   id?: string;
@@ -30,6 +47,7 @@ export interface Producto {
 })
 export class InventarioPage implements OnInit {
   productos: Producto[] = [];
+<<<<<<< HEAD
   categorias: any[] = []; // Almacenará las categorías disponibles
   proveedores: any[] = []; // Almacenará los proveedores disponibles
   isEditModalOpen = false; // Controla la visibilidad del modal
@@ -59,6 +77,38 @@ export class InventarioPage implements OnInit {
     this.loadProductos();
     this.loadCategorias();
     this.loadProveedores();
+=======
+  categorias: string[] = [];
+  proveedores: string[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private navCtrl: NavController,
+    private router: Router,
+    private firestore: AngularFirestore,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private categoriaService: CategoriaService,  // Instanciamos el servicio de categorías
+    private proveedoresService: ProveedoresService   // Instanciamos el servicio de proveedores
+  ) {}
+
+  ngOnInit() {
+    // Obtener productos
+    this.productService.getProducts().subscribe((data) => {
+      this.productos = data;
+    });
+
+    // Obtener categorías y proveedores
+    this.categoriaService.getCategorias().subscribe((data) => {
+      this.categorias = data;
+    });
+
+    this.proveedoresService.getProveedores().subscribe((data) => {
+      this.proveedores = data;
+    });
+  }
+
+>>>>>>> parent of ad224c2 (Mejora de page inventario)
   loadProductos() {
     this.productService.getProducts().subscribe((data) => {
       this.productos = data; // Recarga los productos en la lista
@@ -68,6 +118,7 @@ export class InventarioPage implements OnInit {
   goToHome() {
     this.navCtrl.navigateRoot('/home');
   }
+<<<<<<< HEAD
 
 
   // Cargar productos desde Firebase
@@ -132,6 +183,10 @@ export class InventarioPage implements OnInit {
       });
   }
 
+=======
+
+  async editProducto(producto: Producto) {
+>>>>>>> parent of ad224c2 (Mejora de page inventario)
     const alert = await this.alertController.create({
       header: 'Editar Producto',
       inputs: [
